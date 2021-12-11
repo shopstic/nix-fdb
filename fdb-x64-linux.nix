866768081518dcc934a5d7d3cad17eba33a4624b
Jacky Nguyen <nktpro@gmail.com>
2021-12-10 23:45:26 -0500

    Fix lib path

--- fdb-x64-linux.nix
+++ fdb-x64-linux.nix
@@ -18,10 +18,10 @@ rec {
   };
 
   installPhase = ''
-    mkdir -p $out/bin $out/lib $lib/lib
+    mkdir -p $out/bin $out/lib $lib
     find . -type f -not -path "*.sha256" -exec cp {} $out/bin/ \;
     cp ${fdbLib} $out/lib/libfdb_c_${version}.so
-    cp ${fdbLib} $lib/lib/libfdb_c_${version}.so
+    cp ${fdbLib} $lib/libfdb_c_${version}.so
   '';
 
   outputs = [ "out" "lib" ];
